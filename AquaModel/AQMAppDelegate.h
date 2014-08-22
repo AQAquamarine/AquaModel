c46f4d96559b6946b8d81b068ac4d53c376b82bc
Kai INUI <lied.der.optik@gmail.com>
2014-08-22 22:57:24 +0900

    init

---
+++ AquaModel/AQMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AQMAppDelegate.h
+//  AquaModel
+//
+//  Created by kaiinui on 2014/08/22.
+//  Copyright (c) 2014年 Aquamarine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AQMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
